cd9b712eb0f65efdc3b283d2160d7f655eabe6ae
Xing Yongwang <xyw@localhost.localdomain>
2024-07-13 19:49:55 +0800

    add

---
+++ push.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# 写一个git的py
+#
+import os
+import sys
+import subprocess
+
+def git_push():
+    path = os.path.abspath(os.path.dirname(__file__))
+    os.chdir(path)
+    os.system('git add .')
+    os.system('git commit -m "add"')
+    os.system('git push -u origin main')
+    print('push success')
+
+if __name__ == '__main__':
+    git_push()
