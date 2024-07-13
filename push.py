9c879fb406f6ed5ac94cf5b1a83cf97ccfab28b3
Xing Yongwang <xyw@localhost.localdomain>
2024-07-13 19:56:42 +0800

    重新更新了目录

--- push.py
+++ push.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # 写一个git的py
-#
 import os
 import sys
 import subprocess
 
 def git_push():
     path = os.path.abspath(os.path.dirname(__file__))
-    print(path)
     os.chdir(path)
     os.system('git add .')
-    # os.system('git commit -m "add"')
     os.system('git commit -m "%s"' % sys.argv[1])
     os.system('git push -u origin main')
     print('push success')
